ef242c951ef4b4ad025624d13b61f0281d740072
Margot Monge <margotmge@MacBook-Air-de-Margot.local>
2023-04-07 15:48:48 +0200

    ERC721 VotingToken contract added

--- contracts/VotingToken.sol
+++ contracts/VotingToken.sol
@@ -1,38 +1,39 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.9;
 
-import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
-import "@openzeppelin/contracts/token/ERC20/extensions/draft-ERC20Permit.sol";
-import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Votes.sol";
-import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Burnable.sol";
+import "@openzeppelin/contracts/access/AccessControl.sol";
+import "@openzeppelin/contracts/utils/cryptography/draft-EIP712.sol";
+import "@openzeppelin/contracts/token/ERC721/extensions/draft-ERC721Votes.sol";
 
-contract VotingToken is ERC20, ERC20Permit, ERC20Votes, Ownable {
-    constructor() ERC20("VoteToken", "VOTE") ERC20Permit("VoteToken") {}
+contract VotingToken is ERC721, ERC721Burnable, AccessControl, EIP712, ERC721Votes {
+    bytes32 public constant MINTER_ROLE = keccak256("MINTER_ROLE");
+
+    constructor() ERC721("Dauphine DAO", "DTK") EIP712("Dauphine DAO", "1") {
+        _grantRole(DEFAULT_ADMIN_ROLE, msg.sender);
+        _grantRole(MINTER_ROLE, msg.sender);
+    }
+
+    function safeMint(address to, uint256 tokenId) public onlyRole(MINTER_ROLE) {
+        _safeMint(to, tokenId);
+    }
 
     // The following functions are overrides required by Solidity.
 
-    function _afterTokenTransfer(address from, address to, uint256 amount)
+    function _afterTokenTransfer(address from, address to, uint256 tokenId, uint256 batchSize)
         internal
-        override(ERC20, ERC20Votes)
+        override(ERC721, ERC721Votes)
     {
-        super._afterTokenTransfer(from, to, amount);
+        super._afterTokenTransfer(from, to, tokenId, batchSize);
     }
 
-    function _mint(address to, uint256 amount)
-        internal
-        override(ERC20, ERC20Votes)
+    function supportsInterface(bytes4 interfaceId)
+        public
+        view
+        override(ERC721, AccessControl)
+        returns (bool)
     {
-        super._mint(to, amount);
-    }
-
-    function _burn(address account, uint256 amount)
-        internal
-        override(ERC20, ERC20Votes)
-    {
-        super._burn(account, amount);
-    }
-
-    function mintAndDelegate(address to, uint amount) public {
-        _mint(to, amount);
+        return super.supportsInterface(interfaceId);
     }
 }
